c681a2960bbc72b073d3f724844ca5fc70077d19
lisscodes <lisandra.dias008@gmail.com>
2022-05-05 16:15:24 -0300

    API Rest, CRUD completo e security básica

---
+++ src/main/java/comgamestore/gamestore/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package comgamestore.gamestore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import comgamestore.gamestore.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
